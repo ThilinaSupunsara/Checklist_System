afbd230cadfa9f7f6502bc87d0fbf32fab58a5bf
ThilinaSupunsara <thilinasupunsara2001@gmail.com>
2025-10-28 17:21:54 +0530

    log

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('auth\login');
+    return view('auth.login');
 });
 
 
