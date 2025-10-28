a4eb7615363122e47c3d057c8043bc436e3bdee8
ThilinaSupunsara <thilinasupunsara2001@gmail.com>
2025-10-28 18:21:23 +0530

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,17 @@ namespace App\Providers;
 
 use App\Models\Property;
 use App\Policies\PropertyPolicy;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+{
+    if ($this->app->environment('production')) {
+        URL::forceScheme('https');
+    }
+}
     /**
      * Register any application services.
      */
@@ -19,10 +26,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
-    }
+
     protected $policies = [
     // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     Property::class => PropertyPolicy::class, // Add this line
